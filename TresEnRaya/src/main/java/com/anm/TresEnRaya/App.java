46a58d67933c32226560e200ef54ca249560e493
AlbertNoMes <albertnotariomestres@gmail.com>
2022-02-15 09:12:48 +0100

    commit 1

---
+++ TresEnRaya/src/main/java/com/anm/TresEnRaya/App.java
@@ -0,0 +1,13 @@
+package com.anm.TresEnRaya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
